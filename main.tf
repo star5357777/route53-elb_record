e4c16293f8213e5646b5ef1b58b28df964904b62
star5357777 <108916391+star5357777@users.noreply.github.com>
2023-03-20 14:16:33 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,17 @@
 resource "aws_route53_zone" "route53_zone" {
-  name = var.zone_name
+        for_each = var.names
+        name = each.value.name
 }
 
-resource "aws_route53_record" "elb_record" {
-  zone_id = aws_route53_zone.route53_zone.zone_id
-  name = var.record_name
-  type = "A"
-  
-  alias {
-    name = data.aws_lb.elb_data.dns_name
-    zone_id = data.aws_lb.elb_data.zone_id
-    evaluate_target_health = true
-  }
-}
+resource "aws_route53_record" "route53_record" {
+        for_each = var.names
+        zone_id = aws_route53_zone.route53_zone[each.key].zone_id
+        name = each.value.record_name
+        type = "A"
 
-output "route53_ns" {
-  value = aws_route53_zone.route53_zone.name_servers
+        alias {
+                name = data.aws_lb.elb_data.dns_name
+                zone_id = data.aws_lb.elb_data.zone_id
+                evaluate_target_health = true
+        }
 }
