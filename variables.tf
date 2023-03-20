3a106d01bb3013cd67e82484e21330c0b81d7c65
star5357777 <108916391+star5357777@users.noreply.github.com>
2023-03-20 14:17:06 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,9 @@ variable "secret_key" {
   type = string
 }
 
-variable "zone_name" {
-  type = string
-}
-
-variable "record_name" {
-  type = string
+variable "names" {
+        type = map(object({
+                name = string
+                record_name = string
+        }))
 }
